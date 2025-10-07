525369be261caf2400f0aae07bdc2bd4b9b6f4f3
ayushkumar59888-collab <ayushkumar59888@gmail.com>
2025-10-07 19:50:02 +0530

    Create 17.java

---
+++ 17.java
@@ -0,0 +1,17 @@
+public class FarmersBasket {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30};
+        int key = 25;
+
+        int index = findFruit(arr, key);
+        System.out.println(index);
+    }
+
+    
+    public static int findFruit(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) return i; 
+        }
+        return -1; 
+    }
+}
