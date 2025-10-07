7855800d51d41ca82e486709b6e916ede39ebfbe
ayushkumar59888-collab <ayushkumar59888@gmail.com>
2025-10-07 19:48:02 +0530

    Create 15.java

---
+++ 15.java
@@ -0,0 +1,12 @@
+public class HiddenChamber {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        int sum = sumArray(arr, arr.length);
+        System.out.println(sum);
+    }
+
+    public static int sumArray(int[] arr, int n) {
+        if (n == 0) return 0;        
+        return sumArray(arr, n - 1) + arr[n - 1]; 
+    }
+}
