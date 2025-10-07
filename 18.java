8937caf2ab1eea3ac05fec5c150070bd8c0f0611
ayushkumar59888-collab <ayushkumar59888@gmail.com>
2025-10-07 19:51:05 +0530

    Create 18.java

---
+++ 18.java
@@ -0,0 +1,23 @@
+public class SecretDoor {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 7, 9};
+        int key = 7;
+
+        int index = binarySearch(arr, key);
+        System.out.println(index);
+    }
+    public static int binarySearch(int[] arr, int key) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == key) return mid;  
+            else if (arr[mid] < key) left = mid + 1;  
+            else right = mid - 1;
+        }
+
+        return -1;
+    }
+}
